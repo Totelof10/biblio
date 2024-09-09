51bedae1c70840c69f7e6452b8f53793e151a735
Tohaina <rtohainamanoa@gmail.com>
2024-09-09 17:39:20 +0300

    Initial commit

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_biblio_FILE C:/Users/rtoha/Documents/biblio/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/biblio.exe)
+set(__QT_DEPLOY_TARGET_biblio_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_biblio_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
